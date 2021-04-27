3a333f22f804951a3603f44c89c7514bf41a676d
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-04-27 21:45:40 +0800

    尝试部署

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+docker rm -f EDoctor_django&&
+docker rm -f EDoctor_vue&&
+docker rmi -f edoctor_django&&
+docker rmi -f edoctor_vue&&
+git pull&&
+docker build -t edoctor_django .&&
+docker run -it --name EDoctor_django -p 8020:8020 -d edoctor_django&&
+docker exec -it EDoctor_django /bin/bash&&
 python manage.py makemigrations&&
 python manage.py migrate&&
-uwsgi   --enable-threads /var/www/html/EDoctor/uwsgi.ini
+uwsgi   --enable-threads /var/www/html/EDoctor/uwsgi.ini&&
+exit&&
+docker build -t edoctor_vue .&&
+docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue&&
