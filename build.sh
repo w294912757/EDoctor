c11c7a4570b4d38fe731cb8eed1c320baede97d5
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-04-27 22:04:16 +0800

    尝试部署

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ docker rm -f EDoctor_vue&&
 docker rmi -f edoctor_django&&
 docker rmi -f edoctor_vue&&
 git pull&&
+cd frontend&&
 docker build -t edoctor_vue .&&
 docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue&&
+cd ..&&
 docker build -t edoctor_django .&&
 docker run -it --name EDoctor_django -p 8020:8020 -d edoctor_django&&
 docker exec -it EDoctor_django /bin/bash
