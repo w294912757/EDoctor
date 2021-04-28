80d40f7ea6dc89321b80ba9f801699f762b2645c
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-04-28 22:14:47 +0800

    尝试部署

--- vue.sh
+++ vue.sh
@@ -4,4 +4,5 @@ cd frontend&&
 npm install&&
 npm run build&&
 docker build -t edoctor_vue .&&
-docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue
+docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue&&
+cd ..
