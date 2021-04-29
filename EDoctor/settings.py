db553b5be52094431e27836cca26e6085ee29f44
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-04-29 11:45:05 +0800

    尝试部署

--- EDoctor/settings.py
+++ EDoctor/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 APPEND_SLASH = False
 
 UPLOAD_FILE = os.path.join(BASE_DIR, 'images')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
-MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
