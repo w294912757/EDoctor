1790a1274c70fd8ba4d7831547f83aa1bdb18bc9
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-04-29 11:52:53 +0800

    尝试部署

--- django.sh
+++ django.sh
@@ -2,3 +2,4 @@
 git pull&&
 docker build -t edoctor_django .&&
 docker run -it --name EDoctor_django -p 8020:8020 -d edoctor_django
+sudo docker exec -it EDoctor_django /bin/bash start.sh
--- server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo docker exec -it EDoctor_django /bin/bash start.sh
