e6e39c854af6f56448d021eea5d1a02b7d520f30
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-05-01 03:12:27 +0800

    设置nginx

--- vue.sh
+++ vue.sh
@@ -2,5 +2,5 @@
 git pull&&
 cd frontend&&
 docker build -t edoctor_vue .&&
-docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue&&
+docker run -it --name EDoctor_vue -p 80:800 -d edoctor_vue&&
 cd ..
