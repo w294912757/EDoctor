5a8072af86085e4670dd5c947bbdd92ebf28fcfe
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-05-01 21:06:00 +0800

    更改静态文件收集顺序

--- django.sh
+++ django.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 git pull&&
 docker build -t edoctor_django .&&
-docker run -it --name EDoctor_django -p 8000:8000 -p 8020:8020 -d edoctor_django&&
-sudo docker exec -it EDoctor_django /bin/bash start.sh
+docker run -it --name EDoctor_django -p 8000:8000 -p 8020:8020 -d edoctor_django
--- vue.sh
+++ vue.sh
@@ -3,4 +3,5 @@ git pull&&
 cd frontend&&
 docker build -t edoctor_vue .&&
 docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue&&
-cd ..
+cd ..&&
+sudo docker exec -it EDoctor_django /bin/bash start.sh
