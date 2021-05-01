76bf87a2c6480822332089ed3c283a50333f0eda
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-05-01 21:16:26 +0800

    稳定版本

--- vue.sh
+++ vue.sh
@@ -3,5 +3,4 @@ git pull&&
 cd frontend&&
 docker build -t edoctor_vue .&&
 docker run -it --name EDoctor_vue -p 80:80 -d edoctor_vue&&
-cd ..&&
-sudo docker exec -it EDoctor_django /bin/bash start.sh
+cd ..
