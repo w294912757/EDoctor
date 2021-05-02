a04ff4b2bdedba78e57018738f9b597464f7f522
w294912757 <6503963+w294912757@user.noreply.gitee.com>
2021-05-02 20:59:42 +0800

    echarts绘图

--- EDoctor/settings.py
+++ EDoctor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd$cct^ovqgw31ji9pm&bznm_+(k2u$6ysd!#b8&@5_=nfyd13b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # 部署时设置为False
 
 ALLOWED_HOSTS = ['*']
